6be5f584a9d615b6badebbe599b124cbdbe968a5
nv21071-Moayyed-Ahmed <NV21071@nvtc.edu.bh>
2022-12-26 10:30:24 +0300

    updated title

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -264,8 +264,8 @@ export default function Home({ articles }) {
             Software designer, Cloud Artifact 11CCP.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Moayyed, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
+            I’m Moayyed, a software designer and entrepreneur based in Bahrain
+            . I’m the founder and CEO of Planetaria, where we develop
             technologies that empower regular people to explore space on their
             own terms.
           </p>
